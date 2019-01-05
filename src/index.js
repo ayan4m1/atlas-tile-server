f1d98f3e2ec3a5c805302082f86fee8192bb7097
ayan4m1 <andrew@bulletlogic.com>
2019-01-05 11:20:19 -0500

    change from png to webp output

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import tilestrata from 'tilestrata';
 import disk from 'tilestrata-disk';
+import sharp from 'tilestrata-sharp';
 
 import config from './config';
 
@@ -13,8 +14,9 @@ const server = tilestrata();
 
 server
   .layer('base')
-  .route('*.png')
-  .use(disk.provider(join(basePath, '{z}', '{x}', '{y}.png')));
+  .route('*.webp')
+  .use(disk.provider(join(basePath, '{z}', '{x}', '{y}.png')))
+  .use(sharp(image => image.webp()));
 
 console.log(`Listening on http://${address}:${port}/\n`);
 server.listen(port, address);
